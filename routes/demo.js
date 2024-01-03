ae6c19611c74ddc12f98cb590f10a8c135f911d6
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2024-01-03 11:47:13 -0500

    using sessions & cookies for controlling access

--- routes/demo.js
+++ routes/demo.js
@@ -72,12 +72,17 @@ router.post('/login', async function (req, res) {
     console.log('Could not log in = passwords are not equal!');
     return res.redirect('/login')
   }
-  console.log('User is authenticated!');
-  res.redirect('/admin')
-
+  req.session.user = { id: existingUser._id, email: existingUser.email};
+  req.session.isAuthenticated = true;
+  req.session.save(function() {
+    res.redirect('/admin')
+  });
 });
 
 router.get('/admin', function (req, res) {
+  if (!req.session.isAuthenticated) { // if (!req.session.user)
+    return res.status(401).render('401');
+  }
   res.render('admin');
 });
 
